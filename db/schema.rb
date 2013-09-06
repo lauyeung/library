9fdcaf11f58a95390733f03963b0521b4a71f072
Louise Au Yeung <louise@eabx.org>
2013-09-06 10:30:41 -0400

    created categories and categorizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130905193355) do
+ActiveRecord::Schema.define(version: 20130906142818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,27 @@ ActiveRecord::Schema.define(version: 20130905193355) do
     t.datetime "updated_at"
     t.string   "author"
     t.integer  "rating"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "category_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categorizations", force: true do |t|
+    t.integer  "book_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.string   "borrower"
+    t.datetime "checkout"
+    t.datetime "duedate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+list_of_books = [
+  { title: "The Remaining", author: "D.J. Molles", rating: "10"},
+  { title: "Learn to Program", author: "Chris Pine", rating: "9"},
+  { title: "The Well-Grounded Rubyist", author: "David Black", rating: "8"},
+]
+
+list_of_books.each do |attributes|
+  book = Book.where({
+    title: attributes[:title],
+    author: attributes[:author],
+    rating: attributes[:rating]
+    }).first
+
+  if book.nil?
+    book = Book.new(attributes)
+  else
+    book.attributes = attributes
+  end
+
+  book.save!
+
+end
+
+
+list_of_categories = [
+  {category_name: "Fiction: Zombie"},
+  {category_name: "Non-Fiction: Ruby"},
+]
+
+list_of_categories.each do |attributes|
+  category = Category.where({
+    category_name: attributes[:category_name]
+    }).first
+
+  if category.nil?
+    category = Category.new(attributes)
+  else
+    category.attributes = attributes
+  end
+
+  category.save!
+end
