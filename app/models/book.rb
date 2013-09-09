1d89de3eb5b0fbe3d29cbf0815d6205fc2b91485
Louise Au Yeung <louise@eabx.org>
2013-09-08 20:27:40 -0400

    fixed my join table per sams recommendations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ActiveRecord::Base
   validates :rating, numericality: { only_integer: true }
   validates :rating, :inclusion => 0..100
 
+
 end
---
+++ test/models/books_categories_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BooksCategoriesTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
